443defa1b40326923bae27cc32ee223088658a33
Obuli <raaj-obuli@users.noreply.github.com>
2020-01-08 22:55:57 +0530

    desktop template initital commit

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 var MENU_GROUP            = 'menu_group',
     MENU_GROUP_CLASS      = '.' + MENU_GROUP,
+    ANCHOR                = 'a',
     H2                    = 'h2',
     MENU_GROUP_ITEM       = 'menu_group_item',
     MENU_GROUP_ITEM_CLASS = '.' + MENU_GROUP_ITEM,
     ACTIVE                = 'active',
     ACTIVE_CLASS          = '.' + ACTIVE,
+    MENU_TABS             = 'menu_tabs',
+    MENU_TABS_ID          = '#' + MENU_TABS,
     CLICK                 = 'click';
 
 !(function name(window, $) {
-    var _menuGroupElm = $(MENU_GROUP_CLASS);
+    var _menuGroupElm = $(MENU_GROUP_CLASS),
+        _menuTab      = $(MENU_TABS_ID);
 
     $(function() {
+
+        /* mobile menu accordion */
         _menuGroupElm.on(CLICK, H2, function() {
             var _this = $(this);
 
@@ -24,5 +30,24 @@ var MENU_GROUP            = 'menu_group',
                 .parent(MENU_GROUP_ITEM_CLASS)
                 .toggleClass(ACTIVE);
         });
-    })
+
+        /* desktop menu tabs */
+        _menuTab.on(CLICK, ANCHOR, function(e) {
+            var _this = $(this),
+                target = _this.data('target');
+
+            if (_this.hasClass(ACTIVE) === false) {
+                _menuTab
+                    .find(ACTIVE_CLASS)
+                    .removeClass(ACTIVE);
+            }
+
+            _this.toggleClass(ACTIVE);
+
+            $('#'+target).trigger(CLICK);
+
+            e.preventDefault();
+        });
+        _menuTab.find(ANCHOR).eq(0).trigger(CLICK);
+    });
 })(window, jQuery)
